c316712b1d31afe0ef5b51a619660ac27af77fa7
teUs4Music <110843783+teUs4Music@users.noreply.github.com>
2024-09-30 22:25:47 +0300

    Update test_greetings.py

--- test_greetings.py
+++ test_greetings.py
@@ -3,6 +3,11 @@ from cStringIO import StringIO
 import sys
 import greetings  # Εισαγωγή του αρχείου `greetings.py`
 
+def test_greeting_variable():
+    # Έλεγχος αν η μεταβλητή greeting υπάρχει και περιέχει την τιμή 'Hello, World!'
+    assert hasattr(greetings, 'greeting'), "Variable 'greeting' doesnt exist in greetings.py"
+    assert greetings.greeting == 'Hello Python', "Variable 'greeting' isnt 'Hello Python'"
+
 def test_print_output():
     # Ανακατεύθυνση της εξόδου σε προσωρινή μνήμη
     old_stdout = sys.stdout
